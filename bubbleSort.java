111799cd4264f1df78e47dbcf9b2527140926f22
Jarvis <12934388+jarvismc@users.noreply.github.com>
2017-11-20 22:45:40 +0800

    Add files via upload

---
+++ bubbleSort.java
@@ -0,0 +1,51 @@
+/*
+=================================================================
+                    SORTING
+=================================================================
+*/
+
+/*
+    FILE            :   bubbleSort.java  
+    AUTHOR          :   Jordan
+    COMMENTS        :   
+    LAST MODIFY     :   11 August 2017
+*/
+import java.util.*;
+public class bubbleSort
+{
+public static void main(String args[])
+{
+    int[] numArray = {5,6,8,9,5,3,4};
+    bubbleSort(numArray);
+}       
+public static void bubbleSort(int [] numArray)
+{
+    int n = numArray.length;
+    int temp = 0;
+    int i,j,k;
+
+    for (i = 0; i < n; i++) 
+    {
+        for ( j = 1; j < (n - i); j++) 
+        {
+
+            if (numArray[j - 1] > numArray[j]) 
+            {
+                temp = numArray[j - 1];
+                numArray[j - 1] = numArray[j];
+                numArray[j] = temp;
+            }
+            for (k=0; k<n; k++) //to show the process of sorting line by line
+            {
+                System.out.print(numArray[k]);
+                System.out.print(' ');
+              
+            }
+                System.out.println();
+                
+        }
+      
+    }
+  System.out.print("Sorted numbers " + Arrays.toString(numArray));
+}
+}
